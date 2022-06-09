d8ed746b91afc8664259224fc03df1206cde0973
JakubStranianek <j.stranianek@gmail.com>
2022-06-09 18:34:43 +0200

    routes + location component

--- art-gallery-page/src/App.js
+++ art-gallery-page/src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
 import Footer from './components/Footer/Footer';
 import Home from './components/Home/Home';
-import Main
- from './components/Main/Main';
+import Main from './components/Main/Main';
+import Location  from "./components/Location/Location";
+
+import {Routes, Route} from "react-router-dom"
+
 function App() {
   return (
     <div>
-      <Home></Home>
-      <Main></Main>
-      <Footer></Footer>
+      <Routes>
+        <Route path='/' element={<><Home/><Main/><Footer/></>}></Route> 
+        <Route path='/location' element={<><Location/></>}></Route> 
+      </Routes>
     </div>
   );
 }
